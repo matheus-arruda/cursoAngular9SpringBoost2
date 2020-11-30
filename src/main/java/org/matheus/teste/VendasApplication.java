59d4d59f76a52fdbd2d3c1f52f7c5ab4943413e5
Matheus Arruda <matheush13@gmail.com>
2020-11-30 14:45:38 -0300

    Atualizações

--- src/main/java/org/matheus/teste/VendasApplication.java
+++ src/main/java/org/matheus/teste/VendasApplication.java
@@ -4,18 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//@SpringBootApplication -> Informa que a classe será gerido pelo SpringBoot
 @SpringBootApplication
+//@RestController -> A aplicação se torna acessível via web
 @RestController
 public class VendasApplication {
-
+    //Metodo com mapeamento de URL
     @GetMapping("/hello")
     public String helloWorld() {
-        return "hello world";
-    }
 
+        return "hello world";
+
+    }
+    //Metodo principal do projeto
     public static void main(String[] args) {
+        //Código para definir qual é a classe que inicia o aplicativo
         SpringApplication.run(VendasApplication.class, args);
+
     }
 
 }
