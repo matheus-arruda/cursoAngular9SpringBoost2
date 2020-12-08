b7590c117658404ff06809ee8f92ec0e07fef241
Matheus Arruda <matheush13@gmail.com>
2020-12-08 11:29:46 -0300

    atts

--- frontend/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ frontend/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -11,9 +11,16 @@ export class ClientesFormComponent implements OnInit {
 
   cliente: Cliente;
 
-  constructor() { }
+  constructor() { 
+    this.cliente = new Cliente();
+  }
 
   ngOnInit(): void {
+  }
+
+
+  onSubmit() {
+    console.log(this.cliente)
   }
 
 }
