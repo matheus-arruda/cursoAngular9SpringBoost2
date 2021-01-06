afc22cce902206a3f422e1e9f2f2da0badf1c22b
Matheus Arruda <matheush13@gmail.com>
2021-01-05 22:19:25 -0300

    atts

--- frontend/clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ frontend/clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -13,6 +13,8 @@ export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[] = []
   servico: ServicoPrestado
+  success: boolean = false;
+  errors: String[];
 
   constructor(
     private clientesService: ClientesService,
@@ -32,9 +34,18 @@ export class ServicoPrestadoFormComponent implements OnInit {
   onSubmit() {
     this.service
     .salvar(this.servico)
-    .subscribe( response => {
-      console.log(response)
-    }) 
+    .subscribe( 
+      //Resposta Ok
+      response => {
+      this.success = true;
+      this.errors = null;
+      this.servico = new ServicoPrestado();
+    }, 
+      //Erro
+      errorResponse => {
+        this.success = false;
+        this.errors = errorResponse.error.errors;
+      })
   }
 
 }
