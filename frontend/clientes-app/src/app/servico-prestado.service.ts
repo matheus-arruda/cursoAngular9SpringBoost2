e7ed5f57011015aede7c84375e96f13272decda2
Matheus Arruda <matheush13@gmail.com>
2021-01-11 14:49:05 -0300

    atts

--- frontend/clientes-app/src/app/servico-prestado.service.ts
+++ frontend/clientes-app/src/app/servico-prestado.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca'
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +16,23 @@ export class ServicoPrestadoService {
 
   salvar(servicoPrestado: ServicoPrestado) : Observable<ServicoPrestado> {
     return this.http.post<ServicoPrestado>(`${this.apiUrl}`, servicoPrestado)
+  }
+
+  buscar(nome:string, mes:number) : Observable<ServicoPrestadoBusca[]> {
+   
+    const httpParams = new HttpParams().set("nome", nome).set("mes", mes.toString());
+
+    if(!nome) {
+      nome = ""
+    }
+
+
+    if(mes) {
+      
+    }
+
+    const url = this.apiUrl + "?" + httpParams.toString()
+
+    return this.http.get<any>(url);
   }
 }
